16499c9310825062b06f258c611a266ff994d00e
Anastasios Grammenos <avgramme@ece.auth.gr>
2021-02-02 05:38:18 +0200

    Created a yaml config file Added more CAN messages

--- src/can_handler.cpp
+++ src/can_handler.cpp
@@ -8,22 +8,25 @@
 #include "libsocketcan.h"
 #include "can_as_dash_aux.h"
 
-#define CHANNEL0    "can0"
+#define CHANNEL0    "vcan0"
 #define CH0BITRATE  1000000 //channel0 bitrate
 
 
 CanHandler::CanHandler(rclcpp::NodeOptions nOpt):Node("CanInterface", "", nOpt)
 {
+    this->loadRosParams();
+    this->pubAndMsgInit();
+
     //initialize can0
-    if (can_do_stop(CHANNEL0) != 0) {
+    if ((CHANNEL0 != "vcan0") && (can_do_stop(CHANNEL0) != 0)) {
         RCLCPP_ERROR(this->get_logger(), "Unable to stop can0.");
         return;
     }
-    if (can_set_bitrate(CHANNEL0, CH0BITRATE) != 0) {
+    if ((CHANNEL0 != "vcan0") && (can_set_bitrate(CHANNEL0, CH0BITRATE) != 0)) {
         RCLCPP_ERROR(this->get_logger(), "Unable to set the bitrate on can0.");
         return;
     }
-    if (can_do_start(CHANNEL0) != 0) {
+    if ((CHANNEL0 != "vcan0") && (can_do_start(CHANNEL0) != 0)) {
         RCLCPP_ERROR(this->get_logger(), "Unable to start can0.");
         return;
     }    
@@ -34,15 +37,14 @@ CanHandler::CanHandler(rclcpp::NodeOptions nOpt):Node("CanInterface", "", nOpt)
     strcpy(this->ifr0.ifr_name, CHANNEL0);
     if (ioctl(this->can0Socket, SIOCGIFINDEX, &this->ifr0) == -1) {
         RCLCPP_ERROR(this->get_logger(), "Unable to perform ioctl for can0.\n");
+        return;
     }
     this->addr0.can_family = AF_CAN;
     this->addr0.can_ifindex = this->ifr0.ifr_ifindex;
     if (bind(this->can0Socket, (struct sockaddr*)&this->addr0, sizeof(this->addr0)) < 0){
         RCLCPP_ERROR(this->get_logger(), "Unable to bind socket with can0.\n");
+        return;
     }
-
-    this->loadRosParams();
-    this->pubAndMsgInit();
 
     RCLCPP_INFO(this->get_logger(), "Can interface initialized");
 
@@ -62,40 +64,75 @@ CanHandler::~CanHandler()
 
 void CanHandler::loadRosParams()
 {
+    this->get_parameter_or<bool>("publishAMI505", this->rosConf.publishAMI505, true);
+    this->get_parameter_or<bool>("publishAux166", this->rosConf.publishAux166, true);
+    this->get_parameter_or<bool>("publishAux336", this->rosConf.publishAux336, true);
     this->get_parameter_or<bool>("publishAux357", this->rosConf.publishAux357, true);
     this->get_parameter_or<bool>("publishAux384", this->rosConf.publishAux384, true);
+    this->get_parameter_or<bool>("publishAux412", this->rosConf.publishAux412, true);
     this->get_parameter_or<bool>("publishDash101", this->rosConf.publishDash101, true);
     this->get_parameter_or<bool>("publishDash107", this->rosConf.publishDash107, true);
     this->get_parameter_or<bool>("publishDash198", this->rosConf.publishDash198, true);
+    this->get_parameter_or<bool>("publishDash204", this->rosConf.publishDash204, true);
     this->get_parameter_or<bool>("publishDash279", this->rosConf.publishDash279, true);
+    this->get_parameter_or<bool>("publishEBS550", this->rosConf.publishEBS550, true);
+    this->get_parameter_or<bool>("publishSWA510", this->rosConf.publishSWA510, true);
 }
 
 void CanHandler::pubAndMsgInit()
 {
+    if (this->rosConf.publishAMI505) {
+        this->pubAMI505 = this->create_publisher<turtle_interfaces::msg::CanAMI505>("can_rotary_switch", 10);
+        this->msgAMI505 = turtle_interfaces::msg::CanAMI505();
+    }
+    if (this->rosConf.publishAux166) {
+        this->pubAux166 = this->create_publisher<turtle_interfaces::msg::CanAux166>("can_brakelight", 10);
+        this->msgAux166 = turtle_interfaces::msg::CanAux166();
+    }
+    if (this->rosConf.publishAux336) {
+        this->pubAux336 = this->create_publisher<turtle_interfaces::msg::CanAux336>("can_ebs_pressure_raw", 10);
+        this->msgAux336 = turtle_interfaces::msg::CanAux336();
+    }
     if (this->rosConf.publishAux357) {
-        this->pubAux357 = this->create_publisher<turtle_interfaces::msg::CanAux357>("can_dash_357", 10);
+        this->pubAux357 = this->create_publisher<turtle_interfaces::msg::CanAux357>("can_rear_hall", 10);
         this->msgAux357 = turtle_interfaces::msg::CanAux357();
     }
     if (this->rosConf.publishAux384) {
-        this->pubAux384 = this->create_publisher<turtle_interfaces::msg::CanAux384>("can_dash_384", 10);
+        this->pubAux384 = this->create_publisher<turtle_interfaces::msg::CanAux384>("can_aux_safe_state", 10);
         this->msgAux384 = turtle_interfaces::msg::CanAux384();
     }
+    if (this->rosConf.publishAux412) {
+        this->pubAux412 = this->create_publisher<turtle_interfaces::msg::CanAux412>("can_fans_pumps", 10);
+        this->msgAux412 = turtle_interfaces::msg::CanAux412();
+    }
     if (this->rosConf.publishDash101) {
-        this->pubDash101 = this->create_publisher<turtle_interfaces::msg::CanDash101>("can_dash_101", 10);
+        this->pubDash101 = this->create_publisher<turtle_interfaces::msg::CanDash101>("can_apps_raw", 10);
         this->msgDash101 = turtle_interfaces::msg::CanDash101();
     }
     if (this->rosConf.publishDash107) {
-        this->pubDash107 = this->create_publisher<turtle_interfaces::msg::CanDash107>("can_dash_107", 10);
+        this->pubDash107 = this->create_publisher<turtle_interfaces::msg::CanDash107>("can_front_hall", 10);
         this->msgDash107 = turtle_interfaces::msg::CanDash107();
     }
     if (this->rosConf.publishDash198) {
-        this->pubDash198 = this->create_publisher<turtle_interfaces::msg::CanDash198>("can_dash_198", 10);
+        this->pubDash198 = this->create_publisher<turtle_interfaces::msg::CanDash198>("can_brake_raw", 10);
         this->msgDash198 = turtle_interfaces::msg::CanDash198();
     }
+    if (this->rosConf.publishDash204) {
+        this->pubDash204 = this->create_publisher<turtle_interfaces::msg::CanDash204>("can_dash_leds", 10);
+        this->msgDash204 = turtle_interfaces::msg::CanDash204();
+    }
     if (this->rosConf.publishDash279) {
-        this->pubDash279 = this->create_publisher<turtle_interfaces::msg::CanDash279>("can_dash_279", 10);
+        this->pubDash279 = this->create_publisher<turtle_interfaces::msg::CanDash279>("can_dash_buttons", 10);
         this->msgDash279 = turtle_interfaces::msg::CanDash279();
-    }    
+    }
+    if (this->rosConf.publishEBS550) {
+        this->pubEBS550= this->create_publisher<turtle_interfaces::msg::CanEBS550>("can_ebs_state", 10);
+        this->msgEBS550 = turtle_interfaces::msg::CanEBS550();
+    } 
+    if (this->rosConf.publishSWA510) {
+        this->pubSWA510 = this->create_publisher<turtle_interfaces::msg::CanSWA510>("can_streering_actual", 10);
+        this->msgSWA510 = turtle_interfaces::msg::CanSWA510();
+    } 
 }
 
 int CanHandler::handleCanReceive()
@@ -103,11 +140,23 @@ int CanHandler::handleCanReceive()
     if (recvfrom(this->can0Socket, &this->recvFrame, sizeof(struct can_frame), MSG_DONTWAIT, (struct sockaddr*)&this->addr0, &this->len) >= 8) {
         ioctl(this->can0Socket, SIOCGSTAMP, &this->recvTime);  //get message timestamp
 
-        if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_357_FRAME_ID && this->rosConf.publishAux357) {
+        if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AMI_505_FRAME_ID && this->rosConf.publishAMI505) {
+            this->publish_ami_505();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_166_FRAME_ID && this->rosConf.publishAux166) {
+            this->publish_aux_166();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_336_FRAME_ID && this->rosConf.publishAux336) {
+            this->publish_aux_336();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_357_FRAME_ID && this->rosConf.publishAux357) {
             this->publish_aux_357();
         }
         else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_384_FRAME_ID && this->rosConf.publishAux384) {
             this->publish_aux_384();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_AUX_412_FRAME_ID && this->rosConf.publishAux412) {
+            this->publish_aux_412();
         }
         else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_DASH_101_FRAME_ID && this->rosConf.publishDash101) {
             this->publish_dash_101();
@@ -118,8 +167,17 @@ int CanHandler::handleCanReceive()
         else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_DASH_198_FRAME_ID && this->rosConf.publishDash198) {
             this->publish_dash_198();
         }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_DASH_204_FRAME_ID && this->rosConf.publishDash204) {
+            this->publish_dash_204();
+        }
         else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_DASH_279_FRAME_ID && this->rosConf.publishDash279) {
             this->publish_dash_279();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_EBS_550_FRAME_ID && this->rosConf.publishEBS550) {
+            this->publish_ebs_550();
+        }
+        else if (this->recvFrame.can_id == CAN_AS_DASH_AUX_SWA_510_FRAME_ID && this->rosConf.publishSWA510) {
+            this->publish_swa_510();
         }
 
         return CAN_READY;
@@ -133,6 +191,48 @@ void CanHandler::createHeader(std_msgs::msg::Header *header)
     header->frame_id = "";
     header->stamp.sec = this->recvTime.tv_sec;
     header->stamp.nanosec = this->recvTime.tv_usec*1000;
+}
+
+void CanHandler::publish_ami_505()
+{
+    can_as_dash_aux_ami_505_t msg;
+    if (can_as_dash_aux_ami_505_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of AMI_505");
+        return;
+    }
+
+    this->createHeader(&this->msgAMI505.header);
+    this->msgAMI505.amimission = msg.ami_mission;
+
+    this->pubAMI505->publish(this->msgAMI505);
+}
+
+void CanHandler::publish_aux_166()
+{
+    can_as_dash_aux_aux_166_t msg;
+    if (can_as_dash_aux_aux_166_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of AUX_166");
+        return;
+    }
+
+    this->createHeader(&this->msgAux166.header);
+    this->msgAux166.brakelight = msg.brakelight;
+
+    this->pubAux166->publish(this->msgAux166);
+}
+
+void CanHandler::publish_aux_336()
+{
+    can_as_dash_aux_aux_336_t msg;
+    if (can_as_dash_aux_aux_336_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of AUX_336");
+        return;
+    }
+
+    this->createHeader(&this->msgAux336.header);
+    this->msgAux336.ebspressureraw = msg.ebs_pressure_raw;
+
+    this->pubAux336->publish(this->msgAux336);
 }
 
 void CanHandler::publish_aux_357()
@@ -162,6 +262,27 @@ void CanHandler::publish_aux_384()
     this->msgAux384.safestate = msg.safe_state;
 
     this->pubAux357->publish(this->msgAux357);
+}
+
+void CanHandler::publish_aux_412()
+{
+    can_as_dash_aux_aux_412_t msg;
+    if (can_as_dash_aux_aux_412_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of AUX_412");
+        return;
+    }
+
+    this->createHeader(&this->msgAux412.header);
+    this->msgAux412.leftpumpsignal = msg.left_pump_signal;
+    this->msgAux412.rightpumpsignal = msg.right_pump_signal;
+    this->msgAux412.accufanspwm = msg.accu_fans_pwm;
+    this->msgAux412.leftfanspwm = msg.left_fans_pwm;
+    this->msgAux412.rightfanspwm = msg.right_fans_pwm;
+    this->msgAux412.accufans = msg.accu_fans;
+    this->msgAux412.leftfans = msg.left_fans;
+    this->msgAux412.rightfans = msg.right_fans;
+
+    this->pubAux412->publish(this->msgAux412);
 }
 
 void CanHandler::publish_dash_101()
@@ -208,6 +329,26 @@ void CanHandler::publish_dash_198()
     this->pubDash198->publish(this->msgDash198);
 }
 
+void CanHandler::publish_dash_204()
+{
+    can_as_dash_aux_dash_204_t msg;
+    if (can_as_dash_aux_dash_204_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of DASH_204");
+        return;
+    }
+
+    this->createHeader(&this->msgDash204.header);
+    this->msgDash204.fanpwm = msg.fan_pwm;
+    this->msgDash204.buzzer = msg.buzzer;
+    this->msgDash204.safestate1 = msg.safe_state_1;
+    this->msgDash204.enableout = msg.enable_out;
+    this->msgDash204.sensorerror = msg.sensor_error;
+    this->msgDash204.scsoftware = msg.sc_software;
+    this->msgDash204.plactive = msg.pl_active;
+
+    this->pubDash204->publish(this->msgDash204);
+}
+
 void CanHandler::publish_dash_279()
 {
     can_as_dash_aux_dash_279_t msg;
@@ -226,4 +367,37 @@ void CanHandler::publish_dash_279()
     this->msgDash279.scstate = msg.sc_state;
 
     this->pubDash279->publish(this->msgDash279);
+}
+
+void CanHandler::publish_ebs_550()
+{
+    can_as_dash_aux_ebs_550_t msg;
+    if (can_as_dash_aux_ebs_550_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of EBS_550");
+        return;
+    }
+
+    this->createHeader(&this->msgEBS550.header);
+    this->msgEBS550.asmsstate = msg.asms_state;
+    this->msgEBS550.tsmsout = msg.tsms_out;
+    this->msgEBS550.ebsengaged = msg.ebs_engaged;
+    this->msgEBS550.ebsisarmed = msg.ebs_is_armed;
+    this->msgEBS550.ebsled = msg.ebs_led;
+    this->msgEBS550.k2state = msg.k2_state;
+
+    this->pubEBS550->publish(this->msgEBS550);
+}
+
+void CanHandler::publish_swa_510()
+{
+    can_as_dash_aux_swa_510_t msg;
+    if (can_as_dash_aux_swa_510_unpack(&msg, this->recvFrame.data, this->recvFrame.can_dlc) != CAN_OK) {
+        RCLCPP_ERROR(this->get_logger(), "Error during unpack of SWA_510");
+        return;
+    }
+
+    this->createHeader(&this->msgSWA510.header);
+    this->msgSWA510.steeringactual = msg.steering_actual;
+
+    this->pubSWA510->publish(this->msgSWA510);
 }
--- src/can_handler.hpp
+++ src/can_handler.hpp
@@ -5,25 +5,39 @@
 #include <linux/can.h>
 #include <net/if.h>
 
+#include "turtle_interfaces/msg/can_ami505.hpp"
+#include "turtle_interfaces/msg/can_aux166.hpp"
+#include "turtle_interfaces/msg/can_aux336.hpp"
 #include "turtle_interfaces/msg/can_aux357.hpp"
 #include "turtle_interfaces/msg/can_aux384.hpp"
+#include "turtle_interfaces/msg/can_aux412.hpp"
 #include "turtle_interfaces/msg/can_dash101.hpp"
 #include "turtle_interfaces/msg/can_dash107.hpp"
 #include "turtle_interfaces/msg/can_dash198.hpp"
+#include "turtle_interfaces/msg/can_dash204.hpp"
 #include "turtle_interfaces/msg/can_dash279.hpp"
+#include "turtle_interfaces/msg/can_ebs550.hpp"
+#include "turtle_interfaces/msg/can_swa510.hpp"
 
 #define CAN_ERROR      -1
+#define CAN_OK          0
 #define CAN_NOT_READY   1
-#define CAN_READY       0
-#define CAN_OK          2
+#define CAN_READY       2
 
 typedef struct {
+    bool publishAMI505;
+    bool publishAux166;
+    bool publishAux336;
     bool publishAux357;
     bool publishAux384;
+    bool publishAux412;
     bool publishDash101;
     bool publishDash107;
     bool publishDash198;
-    bool publishDash279;    
+    bool publishDash204;
+    bool publishDash279;
+    bool publishEBS550;
+    bool publishSWA510;    
 } RosConfig;
 
 class CanHandler : public rclcpp::Node
@@ -43,6 +57,18 @@ class CanHandler : public rclcpp::Node
         void pubAndMsgInit();
         void createHeader(std_msgs::msg::Header *header);
 
+        rclcpp::Publisher<turtle_interfaces::msg::CanAMI505>::SharedPtr pubAMI505;
+        turtle_interfaces::msg::CanAMI505 msgAMI505;
+        void publish_ami_505();
+
+        rclcpp::Publisher<turtle_interfaces::msg::CanAux166>::SharedPtr pubAux166;
+        turtle_interfaces::msg::CanAux166 msgAux166;
+        void publish_aux_166();
+
+        rclcpp::Publisher<turtle_interfaces::msg::CanAux336>::SharedPtr pubAux336;
+        turtle_interfaces::msg::CanAux336 msgAux336;
+        void publish_aux_336();
+
         rclcpp::Publisher<turtle_interfaces::msg::CanAux357>::SharedPtr pubAux357;
         turtle_interfaces::msg::CanAux357 msgAux357;
         void publish_aux_357();
@@ -50,6 +76,10 @@ class CanHandler : public rclcpp::Node
         rclcpp::Publisher<turtle_interfaces::msg::CanAux384>::SharedPtr pubAux384;
         turtle_interfaces::msg::CanAux384 msgAux384;
         void publish_aux_384();
+
+        rclcpp::Publisher<turtle_interfaces::msg::CanAux412>::SharedPtr pubAux412;
+        turtle_interfaces::msg::CanAux412 msgAux412;
+        void publish_aux_412();
 
         rclcpp::Publisher<turtle_interfaces::msg::CanDash101>::SharedPtr pubDash101;
         turtle_interfaces::msg::CanDash101 msgDash101;
@@ -63,9 +93,21 @@ class CanHandler : public rclcpp::Node
         turtle_interfaces::msg::CanDash198 msgDash198;
         void publish_dash_198();
 
+        rclcpp::Publisher<turtle_interfaces::msg::CanDash204>::SharedPtr pubDash204;
+        turtle_interfaces::msg::CanDash204 msgDash204;
+        void publish_dash_204();
+
         rclcpp::Publisher<turtle_interfaces::msg::CanDash279>::SharedPtr pubDash279;
         turtle_interfaces::msg::CanDash279 msgDash279;
         void publish_dash_279();
+
+        rclcpp::Publisher<turtle_interfaces::msg::CanEBS550>::SharedPtr pubEBS550;
+        turtle_interfaces::msg::CanEBS550 msgEBS550;
+        void publish_ebs_550();
+
+        rclcpp::Publisher<turtle_interfaces::msg::CanSWA510>::SharedPtr pubSWA510;
+        turtle_interfaces::msg::CanSWA510 msgSWA510;
+        void publish_swa_510();
 
     public:
         CanHandler(rclcpp::NodeOptions nOpt);
