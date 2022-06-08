ef1d4567769ebfdf895ec1913b5be8ef987496ae
Eleni Papageorgiou <papageeleni@gapps.auth.gr>
2022-06-09 00:17:33 +0300

    sub to state_flowchart_state

--- src/variables_init.hpp
+++ src/variables_init.hpp
@@ -108,7 +108,7 @@ void CanHandler::variablesInit()
     //Initialize CAN Tx messages
     //-------------------------
     if (this->rosConf.transmitApuStateMission || this->rosConf.transmitDvSystemStatus) {
-        this->subApuState = this->create_subscription<turtle_interfaces::msg::StateMachineState>("state_machine_state", serviceQos, std::bind(&CanHandler::apu_state_callback, this, _1));
+        this->subApuState = this->create_subscription<turtle_interfaces::msg::StateMachineState>("state_flowchart_state", serviceQos, std::bind(&CanHandler::apu_state_callback, this, _1));
         this->subApuMission = this->create_subscription<turtle_interfaces::msg::Mission>("current_mission", serviceQos, std::bind(&CanHandler::apu_mission_callback, this, _1));
 
         this->frameApuStateMission.as_mission = CAN_MCU_APU_STATE_MISSION_AS_MISSION_ERROR_MISSION_CHOICE;
