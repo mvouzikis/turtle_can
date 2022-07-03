a8fd35f66a6cc99fa8c62885c92646385df4591e
papageeleni <papageeleni@gapps.auth.gr>
2022-07-03 14:32:03 +0300

    fixes

--- src/can_handler.hpp
+++ src/can_handler.hpp
@@ -229,8 +229,8 @@ class CanHandler : public rclcpp::Node
         void apu_mission_callback(turtle_interfaces::msg::Mission::SharedPtr msgApuMission);
 
         rclcpp::Subscription<turtle_interfaces::msg::MissionStatus>::SharedPtr subMissionStatus;                
-        void apu_set_finished_callback(turtle_interfaces::msg::MissionStatus::SharedPtr msgApuMission);
-
+        void apu_set_finished_callback(turtle_interfaces::msg::MissionStatus::SharedPtr msgMissionStatus);
+        
         rclcpp::Subscription<turtle_interfaces::msg::ActuatorCmd>::SharedPtr subActuatorCmd;        
         void actuator_cmd_callback(turtle_interfaces::msg::ActuatorCmd::SharedPtr msgActuatorCmd);
 
